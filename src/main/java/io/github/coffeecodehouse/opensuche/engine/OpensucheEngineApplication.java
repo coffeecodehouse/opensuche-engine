79a8e7c91b63c626ba7b06228dfcafd22703140a
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-02-04 11:19:54 +0200

    Initial commit

---
+++ src/main/java/io/github/coffeecodehouse/opensuche/engine/OpensucheEngineApplication.java
@@ -0,0 +1,12 @@
+package io.github.coffeecodehouse.opensuche.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensucheEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpensucheEngineApplication.class, args);
+	}
+}
