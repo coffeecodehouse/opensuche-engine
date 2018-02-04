78afad694f81831171cde44bdddb594aa7ab4f16
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-02-04 11:23:41 +0200

    Removed spring boot

--- src/main/java/io/github/coffeecodehouse/opensuche/engine/OpensucheEngineApplication.java
+++ src/main/java/io/github/coffeecodehouse/opensuche/engine/OpensucheEngineApplication.java
@@ -3,10 +3,6 @@ package io.github.coffeecodehouse.opensuche.engine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class OpensucheEngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpensucheEngineApplication.class, args);
-	}
 }
